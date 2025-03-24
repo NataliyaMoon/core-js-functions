54a47bb2240f0471e3568857677ef2ce26e8deea
Moon Natalia <nataliya.moon@gmail.com>
2025-03-24 11:20:14 +0500

    add 10 tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -184,8 +184,14 @@ function retry(func, initialAttempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function logWrapper(...args) {
+    const argsStr = args.map((arg) => JSON.stringify(arg)).join(','); // Removed the space after the comma
+    logFunc(`${func.name}(${argsStr}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argsStr}) ends`);
+    return result;
+  };
 }
 
 /**
